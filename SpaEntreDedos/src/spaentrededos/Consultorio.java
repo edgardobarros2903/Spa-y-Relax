5e552a124f96283d73b5be401b5f103bb7cd9b43
edgardobarros2903 <edgardo.barros29@hotmail.com>
2025-10-21 18:47:03 -0300

    Clase consultorio

---
+++ SpaEntreDedos/src/spaentrededos/Consultorio.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaentrededos;
+
+/**
+ *
+ * @author Valentin Barros
+ */
+public class Consultorio {
+    private int nroConsultorio;
+    private String usos;
+    private String equipamiento;
+    private boolean apto;
+
+    public Consultorio() {
+    }
+
+    public Consultorio(int nroConsultorio, String usos, String equipamiento, boolean apto) {
+        this.nroConsultorio = nroConsultorio;
+        this.usos = usos;
+        this.equipamiento = equipamiento;
+        this.apto = apto;
+    }
+
+    public int getNroConsultorio() {
+        return nroConsultorio;
+    }
+
+    public void setNroConsultorio(int nroConsultorio) {
+        this.nroConsultorio = nroConsultorio;
+    }
+
+    public String getUsos() {
+        return usos;
+    }
+
+    public void setUsos(String usos) {
+        this.usos = usos;
+    }
+
+    public String getEquipamiento() {
+        return equipamiento;
+    }
+
+    public void setEquipamiento(String equipamiento) {
+        this.equipamiento = equipamiento;
+    }
+
+    public boolean isApto() {
+        return apto;
+    }
+
+    public void setApto(boolean apto) {
+        this.apto = apto;
+    }
+    
+    
+    
+}
